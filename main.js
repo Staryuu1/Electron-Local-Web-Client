41700089da60e33e6ce1f8bf56be4c47534e9c5e
M. Dwi Becket <mohdwibecket@gmail.com>
2025-04-05 23:55:44 +0700

    Major Update

--- main.js
+++ main.js
@@ -1,8 +1,16 @@
-// main.js
-const { app, BrowserWindow, dialog } = require('electron');
+const { app, BrowserWindow, dialog, ipcMain } = require('electron');
 const os = require('os');
 const net = require('net');
 const path = require('path');
+const http = require('http');
+
+const unsafePorts = [
+  1, 7, 9, 11, 13, 15, 17, 19, 20, 21, 22, 23, 25, 37, 42, 43, 53,
+  77, 79, 87, 95, 101, 102, 103, 104, 109, 110, 111, 113, 115, 117,
+  119, 123, 135, 139, 143, 179, 389, 427, 465, 512, 513, 514, 515,
+  526, 530, 531, 532, 540, 548, 556, 563, 587, 601, 636, 993, 995,
+  2049, 3659, 4045, 6000, 6665, 6666, 6667, 6668, 6669, 6697
+];
 
 function getLocalIPs() {
   const interfaces = os.networkInterfaces();
@@ -22,7 +30,7 @@ function getLocalIPs() {
   return ips;
 }
 
-function checkPortOpen(ip, port, timeout = 1000) {
+function checkPortOpen(ip, port, timeout = 500) {
   return new Promise((resolve) => {
     const socket = new net.Socket();
     socket.setTimeout(timeout);
@@ -46,7 +54,23 @@ function checkPortOpen(ip, port, timeout = 1000) {
   });
 }
 
-async function scanForLaravel(ports) {
+function fetchTitle(ip, port) {
+  return new Promise((resolve) => {
+    http.get(`http://${ip}:${port}`, (res) => {
+      let data = '';
+      res.on('data', chunk => data += chunk);
+      res.on('end', () => {
+        const match = data.match(/<title>(.*?)<\/title>/i);
+        const title = match ? match[1] : 'Tanpa Judul';
+        resolve({ ip, port, title });
+      });
+    }).on('error', () => {
+      resolve(null);
+    });
+  });
+}
+
+async function scanForServers(ports) {
   const ips = getLocalIPs();
   const checks = [];
 
@@ -56,30 +80,60 @@ async function scanForLaravel(ports) {
     }
   }
 
-  const results = await Promise.all(checks);
-  return results.filter(Boolean);
+  const openPorts = (await Promise.all(checks)).filter(Boolean);
+  const titleChecks = openPorts.map(({ ip, port }) => fetchTitle(ip, port));
+  const resultsWithTitle = (await Promise.all(titleChecks)).filter(Boolean);
+
+  return resultsWithTitle;
 }
 
 async function promptForPorts(win) {
   const result = await dialog.showMessageBox(win, {
     type: 'question',
-    buttons: ['Scan Semua', '8000', '8080', '3000', '5000'],
+    buttons: ['Scan Semua', '8000', '8080', '3000', '5000', 'Custom Port'],
     title: 'Pilih Port',
     message: 'Pilih port yang ingin dipindai:',
     cancelId: 0,
   });
 
+  if (result.response === 5) {
+    return new Promise((resolve) => {
+      const customPortWin = new BrowserWindow({
+        width: 400,
+        height: 300,
+        parent: win,
+        modal: true,
+        webPreferences: {
+          preload: path.join(__dirname, 'preload.js'),
+          contextIsolation: true,
+          nodeIntegration: false,
+        },
+      });
+
+      customPortWin.loadFile('custom-port.html');
+
+      ipcMain.once('submit-custom-port', (event, port) => {
+        customPortWin.close();
+        resolve([port]);
+      });
+
+      ipcMain.once('cancel-custom-port', () => {
+        customPortWin.close();
+        resolve([8000, 8080, 3000, 5000]);
+      });
+
+      customPortWin.on('closed', () => {
+        resolve([8000, 8080, 3000, 5000]);
+      });
+    });
+  }
+
   switch (result.response) {
-    case 1:
-      return [8000];
-    case 2:
-      return [8080];
-    case 3:
-      return [3000];
-    case 4:
-      return [5000];
-    default:
-      return [8000, 8080, 3000, 5000];
+    case 1: return [8000];
+    case 2: return [8080];
+    case 3: return [3000];
+    case 4: return [5000];
+    default: return [8000, 8080, 3000, 5000];
   }
 }
 
@@ -88,39 +142,57 @@ async function createWindow() {
     width: 1000,
     height: 700,
     fullscreen: false,
-    autoHideMenuBar: true,
+    autoHideMenuBar: false,
     webPreferences: {
       preload: path.join(__dirname, 'preload.js'),
       contextIsolation: true,
-      devTools: false
+      nodeIntegration: false,
     },
-  });
-
-  win.webContents.on('before-input-event', (event, input) => {
-    if ((input.control || input.meta) && ['i', 'j', 'k'].includes(input.key.toLowerCase())) {
-      event.preventDefault();
-    }
   });
 
   await win.loadFile('loading.html');
 
   const portsToScan = await promptForPorts(win);
-  const found = await scanForLaravel(portsToScan);
+  const foundList = await scanForServers(portsToScan);
 
-  if (found.length > 0) {
-    const { ip, port } = found[0];
-    win.webContents.send('web-server-found', `${ip}:${port}`);
+  const hasUnsafe = foundList.some(({ port }) => unsafePorts.includes(port));
+  if (hasUnsafe) {
+    return await win.loadFile('unsafe.html');
+  }
 
+  if (foundList.length > 0) {
+    await win.loadFile('menu.html');
     setTimeout(() => {
-      win.loadURL(`http://${ip}:${port}`);
-    }, 3000);
+      win.webContents.send('list-found', foundList);
+    }, 500);
   } else {
     win.loadFile('notfound.html');
   }
 }
 
-app.whenReady().then(createWindow);
+ipcMain.on('open-server', (event, url) => {
+  const win = BrowserWindow.getFocusedWindow();
+  if (win) {
+    win.loadURL(`http://${url}`);
+  }
+});
 
+ipcMain.on('rescan-servers', async (event) => {
+  const win = BrowserWindow.getFocusedWindow();
+  if (win) {
+    const portsToScan = await promptForPorts(win);
+    const foundList = await scanForServers(portsToScan);
+
+    const hasUnsafe = foundList.some(({ port }) => unsafePorts.includes(port));
+    if (hasUnsafe) {
+      return win.loadFile('unsafe.html');
+    }
+
+    win.webContents.send('list-found', foundList);
+  }
+});
+
+app.whenReady().then(createWindow);
 app.on('window-all-closed', () => {
   if (process.platform !== 'darwin') app.quit();
 });
--- preload.js
+++ preload.js
@@ -2,4 +2,9 @@ const { contextBridge, ipcRenderer } = require('electron');
 
 contextBridge.exposeInMainWorld('electronAPI', {
   onServerFound: (callback) => ipcRenderer.on('server-found', callback),
+  onListFound: (callback) => ipcRenderer.on('list-found', (_event, data) => callback(data)),
+  openServer: (url) => ipcRenderer.send('open-server', url),
+  rescanServers: () => ipcRenderer.send('rescan-servers'),
+  submitCustomPort: (port) => ipcRenderer.send('submit-custom-port', port),
+  cancelCustomPort: () => ipcRenderer.send('cancel-custom-port'),
 });
